b40ffc41426d3960c0c2ba4db4e24d67718346db
eddie ortega <23edortega@gmail.com>
2024-08-22 23:11:33 -0500

    excer 4

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -9,32 +9,33 @@ namespace ConsoleApp4
 {
     internal class Program
     {
-        // this is a excercise 4
+        // this is a excercise 4.
         static void Main(string[] args)
         {
-            // This asks the user for their legth of the rectangle amount 
+            // This asks the user for their legth of the rectangle amount. 
             Console.Write("enter the length of the rectangle: ");
             double length = Convert.ToDouble(Console.ReadLine());
             
-            // This asks the user for their width of the rectangle amount
+            // This asks the user for their width of the rectangle amount.
             Console.Write("enter the width of the rectangle: ");
             double width = Convert.ToDouble(Console.ReadLine());
-            //This solves the equation for perimeter by multpilying 2 to length and witdh
+            //This solves the equation for perimeter by multpilying 2 to length and witdh.
             double perimeter = 2 * (length + width);
-            //This solves the equation for area by multpilying the length and witdh
+            //This solves the equation for area by multpilying the length and witdh.
             double area = length * width;
            
-            //This one for space between width of rectangle and Results
+            //This one is for space between width of rectangle and Results.
             Console.WriteLine();
-
+            
+            //This prints out Results.
             Console.WriteLine("Results:");
-            //this prints out Area of the rectangle
+            //this prints out Area of the rectangle.
             Console.WriteLine("Area of the rectangle: " + area);
 
-            //this prints out Perimeter of the rectangle
+            //this prints out Perimeter of the rectangle.
             Console.WriteLine("Perimeter of the rectangle: " + perimeter);
             Console.ReadLine();
-            // I put Console.ReadLine(); because the build would not display for less than a second
+            // I put Console.ReadLine(); because the build would not display for less than a second.
         }
     }
 }
